a0730749b6358596f1653e61031e39731980fcc4
Fitch, Natalie R <Natalie.Fitch@gtri.gatech.edu>
2018-01-08 10:12:51 -0600

    initial setup.py and _init_.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan  8 09:45:36 2018
+
+@author: nfitch3
+"""
+
+from distutils.core import setup
+
+setup(
+    name='Newsgraphing',
+    version='0.1dev',
+    packages=['src'],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+)
